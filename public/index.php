bd08815208bfa076700bc2653cedacb2dd259431
VasylievHennadii <vga.kha@gmail.com>
2020-03-17 01:28:05 +0200

    work check Router

--- public/index.php
+++ public/index.php
@@ -1,15 +1,21 @@
 <?php 
 
-$query = $_SERVER['QUERY_STRING'];
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
 Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts/', ['controller' => 'Posts', 'action' => 'index']);
+Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
 Router::add('', ['controller' => 'Main', 'action' => 'index']);
 
 debug(Router::getRoutes());
+
+if(Router::matchRoute($query)) {
+    debug(Router::getRoute());
+}else{
+    echo '404';
+}
 
 
 
