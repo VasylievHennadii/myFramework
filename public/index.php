588b2bcd5f29e6a77d694875fdf7d662c445af22
VasylievHennadii <vga.kha@gmail.com>
2020-03-17 00:54:35 +0200

    function debug

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 $query = $_SERVER['QUERY_STRING'];
 
 require '../vendor/core/Router.php';
+require '../vendor/libs/functions.php';
 
 Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
 
---
+++ vendor/libs/functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+function debug($arr) {
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+}
+
+
+
+
+
+
+
+?>
