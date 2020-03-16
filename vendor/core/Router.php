55a96d38432112f8e7d1b81eb524510a172f278e
VasylievHennadii <vga.kha@gmail.com>
2020-03-17 00:41:02 +0200

    add methods getRoutes and getRoute

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -18,6 +18,14 @@
         self::$routes[$regexp] = $route;
     }
 
+    public static function getRoutes() {
+        return self::$routes;
+    }
+
+    public static function getRoute() {
+        return self::$route;
+    }
+
 
  }
 
