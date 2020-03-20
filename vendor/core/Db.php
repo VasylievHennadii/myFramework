e09bc626a059558f3f8b400901e91f63f603506f
VasylievHennadii <vga.kha@gmail.com>
2020-03-20 23:00:34 +0200

    method for class Db

--- vendor/core/Db.php
+++ vendor/core/Db.php
@@ -16,6 +16,43 @@ class Db {
         $this->pdo = new \PDO($db['dsn'],$db['user'],$db['pass']);
     }
 
+
+    /**
+     * если в self::$instance ничего не записано, то мы в него запишем объект данного класса new self(в нем будет pdo с подключением)
+     */
+    public static function instance() {
+        if(self::$instance === null) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    /**
+     * метод для выполнения запросов
+     * будет выполняться, когда делается sql запрос, а данные из бд нам не нужны. Нам нужен ответ true/false
+     * @param $sql
+     * 
+     */
+    public function execute($sql) {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute();
+    }
+
+    /**
+     * метод для выполнения запросов
+     * возвращает данные из бд
+     * @param $sql
+     * 
+     */
+    public function query($sql) {
+        $stmt = $this->pdo->prepare($sql);
+        $res = $stmt->execute();
+        if($res !== false){
+            return $stmt->fetchAll();
+        }
+        return [];
+    }
+
 }
 
 
