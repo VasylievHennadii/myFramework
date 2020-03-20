bac45a6f8e14f9a21d955d45efbfc49a1d18f5cc
VasylievHennadii <vga.kha@gmail.com>
2020-03-20 22:33:37 +0200

    syntax error fixed

--- vendor/core/Db.php
+++ vendor/core/Db.php
@@ -13,7 +13,7 @@ class Db {
 
     protected function __construct() {
         $db = require ROOT . '/config/config_db.php';
-        $this->pdo = new \PDO($db['dsn'],$db['user'],$db['pass'])
+        $this->pdo = new \PDO($db['dsn'],$db['user'],$db['pass']);
     }
 
 }
