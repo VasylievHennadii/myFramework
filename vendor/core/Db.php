11b7588a3d744d80be28d48b10c06233147e6286
VasylievHennadii <vga.kha@gmail.com>
2020-03-20 22:30:34 +0200

    created a private database connection

--- vendor/core/Db.php
+++ vendor/core/Db.php
@@ -8,6 +8,14 @@ namespace vendor\core;
  */
 class Db {
 
+    protected $pdo;
+    protected static $instance;
+
+    protected function __construct() {
+        $db = require ROOT . '/config/config_db.php';
+        $this->pdo = new \PDO($db['dsn'],$db['user'],$db['pass'])
+    }
+
 }
 
 
