6db7445e7247df49cef0c832723aa68e46a0877b
VasylievHennadii <vga.kha@gmail.com>
2020-03-25 00:01:06 +0200

    reading from the database

--- public/test.php
+++ public/test.php
@@ -6,11 +6,18 @@ R::setup($db['dsn'],$db['user'],$db['pass'], $options);
 R::freeze(true);//замораживаем структуру таблицы от изменений параметров данных
 // var_dump(R::testConnection());//проверка соединения с БД -> true/false
 
+
 //Create (для примера создаём новую таблицу категорий в нашей БД)
-$cat = R::dispense('category');
-$cat->title = 'Категория 2';//создаем поле title(поле id создается автоматически)
-$id = R::store($cat);//сохраняем изменения(поля) в БД
-var_dump($id);
+// $cat = R::dispense('category');
+// $cat->title = 'Категория 1';//создаем поле title(поле id создается автоматически)
+// $id = R::store($cat);//сохраняем изменения(поля) в БД
+// var_dump($id);
+
+
+//Read(чтение данных)
+// $cat = R::load('category', $id);
+$cat = R::load('category', 2);//чтение данных с $id=2
+print_r($cat);
 
 
 
