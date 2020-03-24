e4ffec3f4c4826320627d5cee44967dbbebd0672
VasylievHennadii <vga.kha@gmail.com>
2020-03-24 23:47:38 +0200

    test work with RedBeanPHP

--- public/test.php
+++ public/test.php
@@ -3,7 +3,17 @@
 require 'rb.php';
 $db = require '../config/config_db.php';
 R::setup($db['dsn'],$db['user'],$db['pass'], $options);
-var_dump(R::testConnection());
+R::freeze(true);//замораживаем структуру таблицы от изменений параметров данных
+// var_dump(R::testConnection());//проверка соединения с БД -> true/false
+
+//Create (для примера создаём новую таблицу категорий в нашей БД)
+$cat = R::dispense('category');
+$cat->title = 'Категория 2';//создаем поле title(поле id создается автоматически)
+$id = R::store($cat);//сохраняем изменения(поля) в БД
+var_dump($id);
+
+
+
 
 
 
