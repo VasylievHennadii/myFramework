18fafbb04f517ca086c7b643a134797e60b8c101
VasylievHennadii <vga.kha@gmail.com>
2020-04-05 04:04:58 +0300

    error logging

--- public/test/error/index.php
+++ public/test/error/index.php
@@ -6,7 +6,7 @@
 
 define("DEBUG", 1);//1-режим разработки; 0-боевой режим
 
-class NotFoundExeption extends Exception {
+class NotFoundException extends Exception {
     public function __construct($message = '', $code = 404) {
         parent::__construct($message, $code);
     }
@@ -27,16 +27,18 @@ class ErrorHandler {
     }
 
     // создаем свой метод обработки ошибок
-    public function errorHandler($errno, $errstr, $errfile, $errline){
+    public function errorHandler($errno, $errstr, $errfile, $errline){       
+        error_log("[" . date('Y-m-d H:i:s') . "] Текст ошибки: {$errstr} | Файл: {$errfile}, | Строка: {$errline}\n-------------\n", 3, __DIR__ . '/errors.log');
         $this->displayError($errno, $errstr, $errfile, $errline);
         // return false;//ошибка передается дальше
         return true;//ошибка не передается дальше
     }
 
     //метод для получения фатальных ошибок
-    public function fatalErrorHandler(){
-        $error = error_get_last();//получаем последнюю ошибку
+    public function fatalErrorHandler(){        
+        $error = error_get_last();//получаем последнюю ошибку       
         if(!empty($error) && $error['type'] & ( E_ERROR | E_PARSE | E_COMPILE_ERROR | E_CORE_ERROR)){
+            error_log("[" . date('Y-m-d H:i:s') . "] Текст ошибки: {$error['message']} | Файл: {$error['file']}, | Строка: {$error['line']}\n-------------\n", 3, __DIR__ . '/errors.log');
             ob_end_clean();//очищаем буфер обмена
             $this->displayError($error['type'], $error['message'], $error['file'], $error['line']);
         }else{
@@ -45,7 +47,8 @@ class ErrorHandler {
     }
 
     //метод для обработки исключений
-    public function exceptionHandler($e){       
+    public function exceptionHandler($e){            
+        error_log("[" . date('Y-m-d H:i:s') . "] Текст ошибки: {$e->getMessage()} | Файл: {$e->getFile()}, | Строка: {$e->getLine()}\n-------------\n", 3, __DIR__ . '/errors.log');      
         $this->displayError('Исключение', $e->getMessage(), $e->getFile(), $e->getLine(), $e->getCode());
     }
 
@@ -77,9 +80,9 @@ new ErrorHandler();
 //     var_dump($e);
 // }
 
-throw new NotFoundExeption('Страница не найдена');
+// throw new NotFoundException('Страница не найдена');
 
-// throw new Exception('Упс, исключение');
+throw new Exception('Упс, исключение');
 
 
 ?>
