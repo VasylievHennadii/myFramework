a2a1afc304144e2ce5f5df5616085728c5a41f77
VasylievHennadii <vga.kha@gmail.com>
2020-04-23 23:49:20 +0300

    created structure class Language

--- vendor/fw/widgets/language/Language.php
+++ vendor/fw/widgets/language/Language.php
@@ -4,6 +4,42 @@ namespace fw\core\Language;
 
 class Language{
 
+    protected $tpl; //путь к шаблону
+    protected $languages; //список языков
+    protected $language; //активный на данный момент язык
+
+    public function __construct(){
+        $this->tpl = __DIR__ . '/lang_tpl.php';
+        $this->run();
+    }
+    
+    /**
+     * в методе run вызывается метод, который вернет вывод языков
+     */
+    protected function run(){
+
+    }
+
+    /**
+     * метод получает из БД список языков
+     */
+    public static function getLanguages(){
+
+    }
+
+    /**
+     * метод получает активный язык
+     */
+    public static function getLanguage($languages){
+
+    }
+
+    /**
+     * метод получает Html код вывода языков
+     */
+    protected function getHtml(){
+        
+    }
 }
 
 
